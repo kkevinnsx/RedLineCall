000cc2ddb0feabbcfdb250a7405412a84ca1df00
kkevinnsx <kevyn13ofc@gmail.com>
2024-10-15 18:00:42 -0300

    error deploy pt2

--- prisma/migrations/20241009224234_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE `user` MODIFY `latitude` VARCHAR(191) NULL,
-    MODIFY `longitude` VARCHAR(191) NULL;
-
--- AlterTable
-ALTER TABLE `viatura` MODIFY `latitude` VARCHAR(191) NULL,
-    MODIFY `longitude` VARCHAR(191) NULL;
