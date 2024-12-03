46a4c8992144b15c68198f997fbbbd719391e166
kkevinnsx <kevyn13ofc@gmail.com>
2024-12-03 01:56:42 -0300

    atualizando front end do usuário

--- src/app/HomePage/page.js
+++ src/app/HomePage/page.js
@@ -1,23 +1,57 @@
+"use client";
+
+import Image from "next/image";
 import styles from "../styles/homePage.module.css";
 import NavBar from "../components/nav";
 import SOSbutton from "../components/SOSbutton";
 import Location from "../components/locationComponent";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
+import policeLoading from "../img/loadingPolice.png";
 
 export default function HomePage() {
+  const [currentStep, setCurrentStep] = useState("Inicial");
+  const [timer, setTimer] = useState(0);
+
   return (
     <>
       <Location />
       <ToastContainer />
       <div className={styles.leftContainer}>
         <div>
-            <h1 className={styles.text}>SISTEMA DE OCORRÊNCIAS E DENÚNCIAS</h1>
+          <h1 className={styles.text}>SISTEMA DE OCORRÊNCIAS E DENÚNCIAS</h1>
         </div>
-        <SOSbutton />
+        <SOSbutton setCurrentStep={setCurrentStep} setTimer={setTimer} />
       </div>
       <div className={styles.rightContainer}>
-        <p className={styles.messageBox} />
+        {currentStep === "Inicial" && (
+          <div className={styles.alertContainer}>
+            <p className={styles.alertTitle}>Precisando de ajuda?</p>
+            <p className={styles.alertSubtitle}>faça um chamado!</p>
+            <Image
+              className={styles.alertLoading}
+              src={policeLoading}
+              alt="RedLine Logo"
+            />
+          </div>
+        )}
+
+        {currentStep === "loading" && (
+          <div className={styles.alertContainer}>
+            <p className={styles.alertTitle}>Esperando....</p>
+            <p className={styles.alertSubtitle}>
+              Tempo: 00:0{timer < 10 ? timer : 5}
+            </p>
+            <div className={styles.loadingCircle}>
+            </div>
+            <Image
+                className={styles.alertLoading}
+                src={policeLoading}
+                alt="RedLine Logo"
+              />
+          </div>
+        )}
       </div>
       <NavBar />
     </>
--- src/app/components/SOSbutton.js
+++ src/app/components/SOSbutton.js
@@ -3,90 +3,96 @@
 import { useEffect, useState } from "react";
 import styles from "../styles/SOSbutton.module.css";
 import { toast } from "react-toastify";
-import useSendLocation from "../utils/sendLocation"; 
+import useSendLocation from "../utils/sendLocation";
 
-export default function SOSbutton() {
-    const [loading, setLoading] = useState(false);
-    const [statusChat, setStatusChat] = useState(false);
-    const [userLocation, setUserLocation] = useState(null);
-    const [message, setMessage] = useState("");
-    const { startSendingLocation, stopSendingLocation } = useSendLocation();
+export default function SOSbutton({ setCurrentStep, setTimer }) {
+  const [loading, setLoading] = useState(false);
+  const [statusChat, setStatusChat] = useState(false);
+  const [userLocation, setUserLocation] = useState(null);
+  const [message, setMessage] = useState("");
+  const { startSendingLocation, stopSendingLocation } = useSendLocation();
 
-    const fetchLocation = () => {
-        if (!navigator.geolocation) {
-            const errorMsg = "Geolocalização não suportada pelo navegador.";
-            setMessage(errorMsg);
-            toast.error(errorMsg);
-            return;
+  const fetchLocation = () => {
+    if (!navigator.geolocation) {
+      const errorMsg = "Geolocalização não suportada pelo navegador.";
+      
+      toast.error(errorMsg);
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        if (latitude && longitude) {
+          setUserLocation({ latitude, longitude });
+          console.log("Localização obtida:", { latitude, longitude });
+        } else {
+          console.error("Latitude ou longitude inválidas:", { latitude, longitude });
         }
+      },
+      (error) => {
+        console.error("Erro ao obter localização:", error);
+        const errorMsg = "Erro ao obter localização. Verifique as permissões.";
+        
+        toast.error(errorMsg);
+      }
+    );
+  };
 
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const { latitude, longitude } = position.coords;
-                if (latitude && longitude) {
-                    setUserLocation({ latitude, longitude });
-                    console.log('Localização obtida:', { latitude, longitude });
-                } else {
-                    console.error("Latitude ou longitude inválidas:", { latitude, longitude });
-                }
-            },
-            (error) => {
-                console.error("Erro ao obter localização:", error);
-                const errorMsg = "Erro ao obter localização. Verifique as permissões.";
-                setMessage(errorMsg);
-                toast.error(errorMsg);
-            }
-        );
-    };
+  const sendInitialLocation = async () => {
+    if (!userLocation || !userLocation.latitude || !userLocation.longitude) {
+      const errorMsg = "Localização indisponível. Verifique permissões.";
+      
+      toast.error(errorMsg);
+      return;
+    }
 
-    const sendInitialLocation = async () => {
-        if (!userLocation || !userLocation.latitude || !userLocation.longitude) {
-            const errorMsg = "Localização indisponível. Verifique permissões.";
-            setMessage(errorMsg);
-            toast.error(errorMsg);
-            return;
-        }
+    try {
+      const response = await fetch("/api/updateLocation", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userLocation),
+      });
 
-        try {
-            const response = await fetch('/api/updateLocation', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(userLocation),
-            });
+      if (!response.ok) {
+        const errorMsg = `Erro ao atualizar localização: ${response.status}`;
+        
+        toast.error(errorMsg);
+      } else {
+        const data = await response.json();
+        console.log("Localização atualizada com sucesso:", data);
+      }
+    } catch (error) {
+      console.error("Erro ao enviar localização inicial", error);
+      const errorMsg = "Erro ao enviar localização inicial.";
+      
+      toast.error(errorMsg);
+    }
+  };
 
-            if (!response.ok) {
-                const errorMsg = `Erro ao atualizar localização: ${response.status}`;
-                setMessage(errorMsg);
-                toast.error(errorMsg);
-            } else {
-                const data = await response.json();
-                console.log('Localização atualizada com sucesso:', data);
-            }
-        } catch (error) {
-            console.error('Erro ao enviar localização inicial', error);
-            const errorMsg = "Erro ao enviar localização inicial.";
-            setMessage(errorMsg);
-            toast.error(errorMsg);
-        }
-    };
+  const UserActiveSOS = async () => {
+    setLoading(true);
+    setCurrentStep("loading");
+    setTimer(0);
 
-    const UserActiveSOS = async () => {
-        setLoading(true);
-        setMessage("");
+    const interval = setInterval(() => {
+        setTimer((prev) => prev + 1);
+    }, 1000);
 
+    setTimeout(async () => {
         try {
             if (!userLocation || userLocation.latitude === null || userLocation.longitude === null) {
                 const errorMsg = "Localização indisponível. Verifique permissões.";
-                setMessage(errorMsg);
+                
                 toast.error(errorMsg);
                 return;
             }
 
             await sendInitialLocation();
 
-            const response = await fetch('/api/sosButton', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
+            const response = await fetch("/api/sosButton", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(userLocation),
             });
 
@@ -103,44 +109,48 @@ export default function SOSbutton() {
                 }
             } else {
                 const errorMsg = data.message || `Erro: ${response.status}`;
-                setMessage(errorMsg);
+                
                 toast.error(errorMsg);
             }
         } catch (error) {
             console.error("Erro ao ativar SOS:", error.message);
             const errorMsg = "Erro ao ativar o SOS!";
-            setMessage(errorMsg);
+            
             toast.error(errorMsg);
-        } finally {
-            setLoading(false);
         }
-    };
+    }, 2000);
 
-    useEffect(() => {
-        if (userLocation) {
-            sendInitialLocation();
-        }
-    }, [userLocation]);
+    setTimeout(() => {
+        clearInterval(interval);
+        setCurrentStep("Inicial");
+    }, 4500);
+};
 
-    useEffect(() => {
-        fetchLocation();
-    }, []);
+  useEffect(() => {
+    if (userLocation) {
+      sendInitialLocation();
+    }
+  }, [userLocation]);
 
-    return (
-        <div>
-            <button
-                type="button"
-                className={styles.sosButton}
-                onClick={UserActiveSOS}
-                disabled={loading}
-            >
-                <h1 className={styles.sosText}>
-                    {loading ? "Carregando..." : "S.O.S"}
-                </h1>
-            </button>
-            <div className={styles.messageContainer}>
-                {message && <p>{message}</p>}
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    fetchLocation();
+  }, []);
+
+  return (
+    <div>
+      <button
+        type="button"
+        className={styles.sosButton}
+        onClick={UserActiveSOS}
+        disabled={loading}
+      >
+        <h1 className={styles.sosText}>
+          S.O.S
+        </h1>
+      </button>
+      <div className={styles.messageContainer}>
+        {message && <p>{message}</p>}
+      </div>
+    </div>
+  );
 }
