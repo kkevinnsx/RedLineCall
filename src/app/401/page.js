a6c888a2d13c88fea17e454261b37cc221a37a19
kkevinnsx <kevyn13ofc@gmail.com>
2024-12-05 00:26:43 -0300

    arrumando estilação da pagina de erro e chat

--- src/app/401/page.js
+++ src/app/401/page.js
@@ -1,12 +1,44 @@
+"use client";
+
 import styles from '../styles/401.module.css';
 import Link from 'next/link';
+import { toast, ToastContainer } from "react-toastify";
+import { useRouter } from 'next/navigation';
+import React, { useState, useEffect } from "react";
 
 export default function error() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const handleLogout = async () => {
+    setLoading(true);
+  
+    try{
+      const response = await fetch('/api/logout', {method: 'POST'});
+  
+      if(!response.ok){
+        throw new Error('Erro ao fazer logout. Tente Novamente.');
+      }
+      router.push('/LogIn');
+    } catch (error) {
+      console.error('Erro ao fazer logout: ', error);
+      toast.error('Erro ao fazer desconectar')
+    } finally {
+      setLoading(false);
+    }
+  }
+  
+
   return (
     <div className={styles.bgContainer}> 
-        <Link href='/api/logout'>
-            <button>LogOut</button>
-        </Link>
+      <button
+        type='button'
+        onClick={handleLogout}
+        className={styles.buttonDesconect}
+      >
+        <p>Ir para o Login.</p>
+      </button>
+
       </div>
   );
 }
